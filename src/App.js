a29d920bb23df5b78ea21b06545dd42fb2e3c4be
Phil Mercurio <mercurio@thyrd.org>
2021-12-02 18:01:59 -0800

    Initial checkin
    This is the initial version, containing the original React
    demos updated to React 17 & hooks.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+/*
+ * Top-level component consisting of a set of buttons, one
+ * for each demo, and the corresponding demo chart.
+ */
+import './App.css';
+import React from 'react';
+import {Routes, Route, NavLink} from 'react-router-dom';
+
+import Dynamic from './components/Dynamic.js';
+import Events from './components/Events.js';
+import Methods from './components/Methods.js';
+import Simple from './components/Simple.js';
+import ModuleDrag from './components/ModuleDrag.js';
+import ModuleChart from './components/ModuleChart.js';
+import License from './components/License.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>zingchart-react Demo</h2>
+        <h6>A simple example of binding data, mutations with methods, and listening to events</h6>
+        <div className="App-buttonbar">
+          <NavLink to="/" className={({isActive}) => isActive ? "App-button-active" : "App-button-plain"}>Simple</NavLink>
+          <NavLink to="/module-chart" className={({isActive}) => isActive ? "App-button-active" : "App-button-plain"}>Module Chart</NavLink>
+          <NavLink to="/module-drag" className={({isActive}) => isActive ? "App-button-active" : "App-button-plain"}>Module Drag</NavLink>
+          <NavLink to="/dynamic" className={({isActive}) => isActive ? "App-button-active" : "App-button-plain"}>Dynamic</NavLink>
+          <NavLink to="/events" className={({isActive}) => isActive ? "App-button-active" : "App-button-plain"}>Events</NavLink>
+          <NavLink to="/methods" className={({isActive}) => isActive ? "App-button-active" : "App-button-plain"}>Methods</NavLink>
+          <NavLink to="/license" className={({isActive}) => isActive ? "App-button-active" : "App-button-plain"}>License</NavLink>
+        </div>
       </header>
+      <Routes>
+        <Route exact path="/" element={<Simple />} />
+        <Route path="/module-chart" element={<ModuleChart />} />
+        <Route path="/module-drag" element={<ModuleDrag />} />
+        <Route path="/dynamic" element={<Dynamic />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/methods" element={<Methods />} />
+        <Route path="/license" element={<License />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/ModuleChart.js
@@ -0,0 +1,27 @@
+/*
+ * Vector map of the 48 US states. Demonstrates explicitly importing
+ * ZingChart modules.
+ */
+import React, {useState} from 'react';
+import 'zingchart/es6';
+import ZingChart from 'zingchart-react';
+import 'zingchart/modules-es6/zingchart-maps.min.js'
+import 'zingchart/modules-es6/zingchart-maps-usa.min.js'
+
+function ModuleChart() {
+  const [config] = useState({
+    shapes: [
+      {
+        type: 'zingchart.maps',
+        options: {
+          name: 'usa',
+          ignore: ['AK', 'HI']
+        }
+      }
+    ]
+  })
+
+  return <ZingChart data={config} height='600px'/>
+}
+
+export default ModuleChart;
